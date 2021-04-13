5a5c327b6927a59b4247eed95af19734b148855f
triveni07-codes <triveni.wadkar@gmail.com>
2021-04-13 23:31:32 +0200

    Configure Controller to allow access from front end

--- src/main/java/com/assignment/moviesapi/api/MovieController.java
+++ src/main/java/com/assignment/moviesapi/api/MovieController.java
@@ -8,8 +8,10 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 public class MovieController implements MovieApi {
 
