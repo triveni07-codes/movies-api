bb0a1f68c69103241a3f8e6427c9b3a0572fcd83
Triveni <triveni.wadkar@gmail.com>
2021-04-15 01:16:54 +0200

    Chore: Fix pmd issues

--- src/main/java/com/assignment/moviesapi/api/MovieApi.java
+++ src/main/java/com/assignment/moviesapi/api/MovieApi.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Api(value = "movies", tags = "Movie API")
 @RequestMapping(value = "/api")
